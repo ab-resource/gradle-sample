350257ed3c349c89492d6e6f4de5f075c535150c
liuwenke <qwfys200@qq.com>
2023-06-01 16:29:42 +0800

    update

---
+++ src/main/java/com/qwfys/sample/Calculator.java
@@ -0,0 +1,12 @@
+package com.qwfys.sample;
+
+/**
+ * @author liuwenke
+ * @since 0.0.1
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/MainTests.java
@@ -0,0 +1,19 @@
+import com.qwfys.sample.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author liuwenke
+ * @since 0.0.1
+ */
+public class MainTests {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+}
